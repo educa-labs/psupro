218decf4b41461051c8e65a9b5fa02b80ea87181
Andres <afcadiz@uc.cl>
2018-03-16 14:07:18 -0300

    feat(devOps): modified front dockerfile and api url for development mode

--- front/src/constants/url.js
+++ front/src/constants/url.js
@@ -1,1 +1,9 @@
-export default 'https://api.tuniversidad.cl';
+let apiUrl;
+if (process.env.NODE_ENV === 'production') {
+  apiUrl = 'https://api.tuniversidad.cl';
+} else if (process.env.NODE_ENV === 'development') {
+  apiUrl = 'http://localhost:3000';
+}
+
+const apiExport = apiUrl;
+export default apiExport;
