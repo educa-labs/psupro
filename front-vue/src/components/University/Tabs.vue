157d841685124aa11b85f034252302ddb686eb78
btkramm <btkramm@uc.cl>
2018-03-25 22:57:07 -0300

    feat (front): University tabs animated

--- front-vue/src/components/University/Tabs.vue
+++ front-vue/src/components/University/Tabs.vue
@@ -1,13 +1,9 @@
 <template>
   <ul class="tabs z-depth-1">
-    <li class="tab" @click="test(0)"><a @click="$router.replace({ name: 'university' })">{{ 'Información' }}</a></li>
-    <li class="tab" @click="test(1)"><a @click="$router.replace({ name: 'careers' })">{{ 'Carreras' }}</a></li>
+    <li class="tab" @click="active = 0"><a @click="$router.replace({ name: 'university' })">{{ 'Información' }}</a></li>
+    <li class="tab" @click="active = 1"><a @click="$router.replace({ name: 'careers' })">{{ 'Carreras' }}</a></li>
 
-    <li class="indicator"
-      :class="{ animatedRight, animatedLeft }"
-      :style="{ right: right + 'px',left: left + 'px'}"
-      ref="indicator"
-    ></li>
+    <li class="indicator" ref="indicator"></li>
   </ul>
 </template>
 
@@ -18,97 +14,65 @@ export default {
   data() {
     return {
       active: 0,
-      mounted: false,
-      right: 0,
-      left: 0,
-      delay: 550,
-      animatedRight: false,
-      animatedLeft: false,
+      duration: 350,
+      delay: 150,
+      tabs: null,
     };
-  },
-  computed: {
-    animated() {
-      return this.rightAnimated || this.leftAnimated;
-    }
   },
   watch: {
     active(current, previous) {
       if (current - previous > 0) {
-        this.animatedRight = true;
-        this.right = this.getRight();
+        this.animate('right');
 
         setTimeout(() => {
-          this.animatedLeft = true;
-          this.left = this.getLeft();
+          this.animate('left');
         }, this.delay);
       } else if (current - previous < 0) {
-        this.animatedLeft = true;
-        this.left = this.getLeft();
+        this.animate('left');
 
         setTimeout(() => {
-          this.animatedRight = true;
-          this.right = this.getRight();
+          this.animate('right');
         }, this.delay);
       }
     },
   },
   methods: {
-    getRight() {
-      let tabs = Array.from(this.$el.children).filter(li => {
-        return li.className === 'tab';
-      });
-      let rightTabs = tabs.slice(this.active + 1, tabs.length);
+    right() {
+      let rightTabs = this.tabs.slice(this.active + 1, this.tabs.length);
 
-      let right = rightTabs.reduce((before, current) => {
+      return rightTabs.reduce((before, current) => {
         return before + current.offsetWidth;
       }, 0);
-
-      return right;
     },
-    getLeft() {
-      let tabs = Array.from(this.$el.children).filter(li => {
-        return li.className === 'tab';
-      });
-      let leftTabs = tabs.slice(0, this.active);
+    left() {
+      let leftTabs = this.tabs.slice(0, this.active);
 
-      let left = leftTabs.reduce((before, current) => {
+      return leftTabs.reduce((before, current) => {
         return before + current.offsetWidth;
       }, 0);
-
-      return left;
     },
-    setRight() {
-      this.right = this.getRight();
+    animate(property) {
+      anime({
+        targets: this.$refs.indicator,
+        duration: this.duration,
+        easing: 'easeInOutQuad',
+        ...(property === 'right'
+          ? { right: this.right() }
+          : { left: this.left() }),
+      });
     },
-    setLeft() {
-      this.left = this.getLeft();
-    },
-    test(index) {
-      this.active = index;
-    },
-    set() {
-      this.setRight();
-      this.setLeft();
-    },
-    jsAnimateRight() {
-      console.log(anime);
+    setRightLeft() {
+      this.$refs.indicator.style.right = `${this.right()}px`;
+      this.$refs.indicator.style.left = `${this.left()}px`;
     },
   },
   mounted() {
-    this.jsAnimateRight();
-    this.mounted = true;
-    this.set();
-    this.$refs.indicator.addEventListener('transitionend', (event) => {
-      if (event.propertyName === 'right') {
-        console.log('right', event)
-        this.animatedRight = false;        
-      } else if (event.propertyName === 'left') {
-        console.log('left', event)
-        this.animatedLeft = false;        
-      }
-
+    this.tabs = Array.from(this.$el.children).filter(li => {
+      return li.className === 'tab';
     });
-    window.addEventListener('resize', this.set);
+
+    this.setRightLeft();
+    window.addEventListener('resize', this.setRightLeft);
   },
 };
 </script>
@@ -128,13 +92,4 @@ export default {
 
     a
       padding: 0
-
-  .indicator
-    will-change: right, left
-
-  .indicator.animatedRight
-    transition: right .25s ease-in-out
-
-  .indicator.animatedLeft
-    transition: left .25s ease-in-out
 </style>
