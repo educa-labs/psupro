5fcd81ee58843ccfd7b58eab48ef549f6fe7595e
Andres <ironcadiz>
2018-04-04 12:11:50 -0300

    fix(api): fixed serializers being removed

---
+++ api/tuniversidad/app/serializers/carreer_serializer.rb
@@ -0,0 +1,7 @@
+# DEPRECATED
+class CarreerSerializer < ActiveModel::Serializer
+  attributes :id, :title,:university_name,:campu_name,:campu_id,:certification, :area_id,:semesters,:price,:area_title,:schedule,:openings,:employability,:income, :university_id,
+             :admission, :last_cut, :description
+
+  has_one :weighing
+end
---
+++ cleanup-docker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# remove exited containers:
+sudo docker ps --filter status=dead --filter status=exited -aq | xargs -r sudo docker rm -v
+    
+# remove unused images:
+sudo docker images --no-trunc | grep '<none>' | awk '{ print $3 }' | xargs -r sudo docker rmi
+
+# remove unused volumes:
+sudo  docker volume ls -qf dangling=true | xargs -r sudo docker volume rm
+
+#clean up compose:
+sudo docker-compose down -v -rmi all
+
