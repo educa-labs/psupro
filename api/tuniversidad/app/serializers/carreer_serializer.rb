3bd2f602fbe66658d6358bc93abfe873664454d9
Andres <ironcadiz>
2018-04-07 15:34:31 -0300

    feat(api): added university to carreer serialization

--- api/tuniversidad/app/serializers/carreer_serializer.rb
+++ api/tuniversidad/app/serializers/carreer_serializer.rb
@@ -1,7 +1,9 @@
-# DEPRECATED
 class CarreerSerializer < ActiveModel::Serializer
-  attributes :id, :title,:university_name,:campu_name,:campu_id,:certification, :area_id,:semesters,:price,:area_title,:schedule,:openings,:employability,:income, :university_id,
+  attributes :id, :title,:university_name,:campu_name,:campu_id,:certification, :area_id,:semesters,
+             :price,:area_title,:schedule,:openings,:employability,:income, :university_id,
              :admission, :last_cut,:degree_type, :description
+
+  belongs_to :university
 
   has_one :weighing
 end
