dbffdb51793af350a70fc4e31d541c41208cc63d
Andres <ironcadiz>
2018-04-07 11:42:25 -0300

    fix(front): eliminated react front

--- front/src/actions/compress.js
+++
@@ -1,7 +0,0 @@
-import { SHOW_LOGIN } from './types';
-
-export function toggleShowLogin() {
-  return {
-    type: SHOW_LOGIN,
-  };
-}
--- front/src/constants/url.js
+++
@@ -1,9 +0,0 @@
-let apiUrl;
-if (process.env.NODE_ENV === 'production') {
-  apiUrl = 'https://api.tuniversidad.cl';
-} else if (process.env.NODE_ENV === 'development') {
-  apiUrl = 'http://localhost:3000';
-}
-
-const apiExport = apiUrl;
-export default apiExport;
