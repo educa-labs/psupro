6cf3bda9dc56791049b94c3f4098ec182fa2ea8e
Felipe Haase <fahaase@uc.cl>
2018-06-08 15:55:07 -0400

    feat(devOps): changes for eb deployment

--- api/tuniversidad/config/puma.rb
+++ api/tuniversidad/config/puma.rb
@@ -9,11 +9,14 @@ threads threads_count, threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests, default is 3000.
 #
-port        ENV.fetch("PORT") { 3000 }
+# port        ENV.fetch("PORT") { 3000 }
+bind "unix:///var/run/puma.sock?umask=0000"
+
+stdout_redirect "/var/log/puma.stdout.log", "/var/log/puma.stderr.log", true
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+environment ENV.fetch("RAILS_ENV") { "production" }
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
